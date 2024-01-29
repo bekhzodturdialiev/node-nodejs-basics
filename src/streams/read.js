ecbfd0a123168abe6c3329601f51c08169603788
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2024-01-29 21:35:55 +0500

    feat: add stream/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { EOL } from 'node:os'
+import { stdout } from 'node:process'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const file = join(__dirname, "files/fileToRead.txt");
+  const fileStream = createReadStream(file, { encoding: 'utf-8' });
+
+  fileStream.on("data", (chunk) => stdout.write(chunk + EOL));
 };
 
 await read();
