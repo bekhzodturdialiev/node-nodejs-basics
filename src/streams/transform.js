7b6356a6b13f485b6c22d490840242e85757a8e5
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2024-01-29 22:09:21 +0500

    feat: add stream/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from "stream";
+import { stdin, stdout } from 'node:process'
+import { log } from 'node:console'
+
+const transform = () => {
+  const stream = new Transform({
+    transform(chunk, _, callback) {
+      const reversedChunk = chunk.toString().split("").reverse().join("");
+      this.push(reversedChunk);
+      callback();
+    },
+  });
+
+  log('You can type now, your text will be logged out transformed')
+
+  stdin.pipe(stream).pipe(stdout);
 };
 
 await transform();
