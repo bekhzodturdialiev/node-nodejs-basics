e628259f14097cfb8a543ba7cc46d77a2e5a0c8e
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2024-01-29 20:27:41 +0500

    feat: add cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import { argv } from "node:process";
+import { log } from "node:console";
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = argv.slice(2);
+
+  const result = args
+    .filter((_, index) => index % 2 === 0)
+    .map(
+      (arg, index) =>
+        `${arg.startsWith("--") ? arg.slice(2) : arg} is ${args[index * 2 + 1]}`
+    )
+    .join(", ");
+
+  log(result);
 };
 
 parseArgs();
