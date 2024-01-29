8e14eaba4d31b9dc90fc2675ce01ae54d3a0e367
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2024-01-29 18:09:33 +0500

    refactor: copy - reformat code & success log

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,20 +1,27 @@
-import { existsSync, cp } from 'node:fs'
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { existsSync, cp } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { log } from "node:console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const copy = async () => {
-    const source = join(__dirname, 'files')
-    const destination = join(__dirname, 'files_copy')
+  const source = join(__dirname, "files");
+  const destination = join(__dirname, "files_copy");
 
-    if(!existsSync(source) || existsSync(destination)){
-        throw new Error('FS operation failed')
+  if (!existsSync(source) || existsSync(destination)) {
+    throw new Error("FS operation failed");
+  }
+
+  const options = { recursive: true };
+  cp(source, destination, options, (err) => {
+    if (err) {
+      throw new Error(err);
+    } else {
+      log("All files are successfully copied");
     }
-
-    const options = { recursive: true }
-    cp(source, destination, options, () => {})
+  });
 };
 
 await copy();
